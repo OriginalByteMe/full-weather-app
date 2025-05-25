90de66732540d345fafd3a961941a6d9ac8bbc15
originalbyteme <noahrijkaard@gmail.com>
2025-05-25 19:05:21 +0800

    Add run commands in the global package.json and web ones

--- apps/web/cypress.config.ts
+++ apps/web/cypress.config.ts
@@ -1,12 +1,13 @@
 import { defineConfig } from "cypress";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 export default defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:3001',
+    baseUrl: process.env.CYPRESS_BASE_URL || 'http://localhost:3001',
     setupNodeEvents(on, config) {
-      // implement node event listeners here
       on('before:browser:launch', (browser, launchOptions) => {
-        // Add more verbose logging for debugging
         if (browser.name === 'chrome' || browser.name === 'edge') {
           launchOptions.args.push('--disable-gpu');
           launchOptions.args.push('--no-sandbox');
